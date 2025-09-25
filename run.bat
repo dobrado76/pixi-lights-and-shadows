6f2c39b9273607aad536df9b2fdf8be3b270db2c
shadodo76 <dominic.branchaud@gmail.com>
2025-09-25 15:32:54 +1000

    Fix

---
+++ run.bat
@@ -0,0 +1,17 @@
+f:
+cd /TheCaldwellLegacy
+npm install
+npm i -D cross-env
+npm run build
+npm run dev
+
+
+rem modify package.json for this:
+rem
+rem "scripts": {
+rem  "dev": "cross-env NODE_ENV=development tsx server/index.ts",
+rem  "start": "cross-env NODE_ENV=production node dist/index.js",
+rem  "build": "vite build && esbuild server/index.ts --platform=node --packages=external --bundle --format=esm --outdir=dist",
+rem  "check": "tsc",
+rem  "db:push": "drizzle-kit push"
+rem },
